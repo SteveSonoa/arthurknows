38aa1820a7e727558f4e4922df7f07c9809c642b
Steve Marshall <stevesonoa@gmail.com>
2018-03-26 08:57:32 -0700

    Updated Navbar component. Added CSS and IMG directories to client/public folder. Updated index.html to always include CSS for navbar & font awesome CDN.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,20 +1,41 @@
 import React from "react";
 
 const Nav = () => (
-  <nav className="navbar navbar-inverse navbar-top">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <button type="button" className="collapsed navbar-toggle">
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" /> <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <a href="/" className="navbar-brand">
-          React Reading List
-        </a>
-      </div>
+  <nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">Logo</a>
     </div>
-  </nav>
+
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+      <ul className="nav navbar-nav navbar-right">
+        <li className="transition-hover"><a href="#"><img src="img/256x256.png" width="50px" className="menuItem img img-responsive" /></a>
+          <p id="transition-hover-content">Today</p>
+      </li>
+        <li className="transition-hover"><a href="#"><img src="img/256x256.png" width="50px" className="menuItem img img-responsive" /></a>
+          <p id="transition-hover-content">This Week</p>
+        </li>
+        <li className="transition-hover"><a href="#"><img src="img/256x256.png" width="50px" className="menuItem img img-responsive" /></a>
+          <p id="transition-hover-content">Settings</p>
+        </li>
+        <li className="transition-hover"><a href="#"><img src="img/256x256.png" width="50px" className="menuItem img img-responsive" /></a>
+          <p id="transition-hover-content">Profile</p>
+        </li>
+        <li className="transition-hover"><a href="#"><img src="img/256x256.png" width="50px" className="menuItem img img-responsive" /></a>
+          <p id="transition-hover-content">Search</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 );
 
 export default Nav;
