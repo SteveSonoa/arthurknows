768315b2423dee6734d759b3366aa7470b6e8a2c
Steve Marshall <stevesonoa@gmail.com>
2018-03-26 21:38:05 -0700

    Finalized Nav component. Added Panel component.

---
+++ client/src/components/Panel/Panel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Panel = ({ children }) => (
+	<div className="panel panel-default">
+		<div className="panel-body">
+			{children}
+		</div>
+	</div>
+);
+
+export default Panel;
