3c6f4bbdcd62f9cacbd36209a18b901303d84107
Steve Marshall <stevesonoa@gmail.com>
2018-03-29 20:34:10 -0700

    Updated all Jumbotron components to Panel components instead.

--- client/src/pages/Arthur/Arthur.js
+++ client/src/pages/Arthur/Arthur.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import DeleteBtn from "../../components/DeleteBtn";
-import Jumbotron from "../../components/Jumbotron";
+import Panel from "../../components/Panel";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
@@ -56,35 +56,37 @@ class Arthur extends Component {
   render() {
     return (
       <Col size="md-4 sm-12">
-        <Jumbotron>
+        <Panel>
           <h1>What Books Should I Read?</h1>
-        </Jumbotron>
-        <form>
-          <Input
-            value={this.state.title}
-            onChange={this.handleInputChange}
-            name="title"
-            placeholder="Title (required)"
-          />
-          <Input
-            value={this.state.author}
-            onChange={this.handleInputChange}
-            name="author"
-            placeholder="Author (required)"
-          />
-          <TextArea
-            value={this.state.synopsis}
-            onChange={this.handleInputChange}
-            name="synopsis"
-            placeholder="Synopsis (Optional)"
-          />
-          <FormBtn
-            disabled={!(this.state.author && this.state.title)}
-            onClick={this.handleFormSubmit}
-          >
-            Submit Book
-          </FormBtn>
-        </form>
+        </Panel>
+        <Panel>
+          <form>
+            <Input
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <Input
+              value={this.state.author}
+              onChange={this.handleInputChange}
+              name="author"
+              placeholder="Author (required)"
+            />
+            <TextArea
+              value={this.state.synopsis}
+              onChange={this.handleInputChange}
+              name="synopsis"
+              placeholder="Synopsis (Optional)"
+            />
+            <FormBtn
+              disabled={!(this.state.author && this.state.title)}
+              onClick={this.handleFormSubmit}
+            >
+              Submit Book
+            </FormBtn>
+          </form>
+        </Panel>
       </Col>
     );
   }
--- client/src/pages/Books/Books.js
+++ client/src/pages/Books/Books.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import DeleteBtn from "../../components/DeleteBtn";
 import Jumbotron from "../../components/Jumbotron";
+import Panel from "../../components/Panel";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
@@ -56,35 +57,37 @@ class Books extends Component {
   render() {
     return (
       <Col size="md-8 sm-12">
-        <Jumbotron>
+        <Panel>
           <h1>What Books Should I Read?</h1>
-        </Jumbotron>
-        <form>
-          <Input
-            value={this.state.title}
-            onChange={this.handleInputChange}
-            name="title"
-            placeholder="Title (required)"
-          />
-          <Input
-            value={this.state.author}
-            onChange={this.handleInputChange}
-            name="author"
-            placeholder="Author (required)"
-          />
-          <TextArea
-            value={this.state.synopsis}
-            onChange={this.handleInputChange}
-            name="synopsis"
-            placeholder="Synopsis (Optional)"
-          />
-          <FormBtn
-            disabled={!(this.state.author && this.state.title)}
-            onClick={this.handleFormSubmit}
-          >
-            Submit Book
-          </FormBtn>
-        </form>
+        </Panel>
+        <Panel>
+          <form>
+            <Input
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <Input
+              value={this.state.author}
+              onChange={this.handleInputChange}
+              name="author"
+              placeholder="Author (required)"
+            />
+            <TextArea
+              value={this.state.synopsis}
+              onChange={this.handleInputChange}
+              name="synopsis"
+              placeholder="Synopsis (Optional)"
+            />
+            <FormBtn
+              disabled={!(this.state.author && this.state.title)}
+              onClick={this.handleFormSubmit}
+            >
+              Submit Book
+            </FormBtn>
+          </form>
+        </Panel>
       </Col>
     );
   }
