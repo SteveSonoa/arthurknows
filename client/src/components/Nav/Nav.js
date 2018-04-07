eb5fc9bad775e4c103d1a55e10073f861e435e20
Steve Marshall <stevesonoa@gmail.com>
2018-04-06 18:52:34 -0700

    Partially installed @blueprint module. Updated Nav to work with Link.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,60 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => (
-  <nav className="navbar navbar-default">
   <div className="container-fluid">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a id="navbar-hover" className="navbar-brand" >Logo</a>
+    <div className="navbar-logo">
+      <a id="navbar-logo" >Logo</a>
     </div>
 
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div className="navbar-menu">
 
 
-      <ul className="nav navbar-nav navbar-right">
-        <li className="navTransition-hover"><i className="far fa-calendar-minus navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Today</p>
+      <ul className="navbar-list">
+        <li className="navTransition-hover"><Link to="/search/daily"><i className="far fa-calendar-minus navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Today</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="far fa-calendar-alt navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">This Week</p>
+        <li className="navTransition-hover"><Link to="/search/weekly"><i className="far fa-calendar-alt navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">This Week</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-users navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Search</p>
+        <li className="navTransition-hover"><Link to="/search/custom"><i className="fas fa-users navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Search</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-user navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">My Profile</p>
+        <li className="navTransition-hover"><Link to="/profile"><i className="fas fa-user navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">My Profile</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-cogs navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Settings</p>
+        <li className="navTransition-hover"><Link to="/settings"><i className="fas fa-cogs navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Settings</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-dollar-sign navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Billing</p>
+        <li className="navTransition-hover"><Link to="/"><i className="fas fa-dollar-sign navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Billing</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-question navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">FAQ</p>
+        <li className="navTransition-hover"><Link to="/"><i className="fas fa-question navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">FAQ</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="far fa-life-ring navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Support</p>
+        <li className="navTransition-hover"><Link to="/"><i className="far fa-life-ring navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Support</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="far fa-comments navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Contact Us</p>
+        <li className="navTransition-hover"><Link to="/contact"><i className="far fa-comments navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Contact Us</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-user-secret navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">About Us</p>
+        <li className="navTransition-hover"><Link to="/about"><i className="fas fa-user-secret navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">About Us</p></Link>
         </li>
-        <li className="navTransition-hover"><i className="fas fa-lock navMenuItem navi"></i>
-          <p id="navTransition-hover-content" className="navp">Log Out</p>
+        <li className="navTransition-hover"><Link to="/"><i className="fas fa-lock navMenuItem navi"></i>
+          <p id="navTransition-hover-content" className="navp">Log Out</p></Link>
         </li>
       </ul>
     </div>
   </div>
-</nav>
 );
 
 export default Nav;
